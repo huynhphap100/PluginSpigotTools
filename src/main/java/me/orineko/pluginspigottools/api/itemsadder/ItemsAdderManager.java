ad3992514e11ae715c1a7df79f1759ed71a0511f
Sitonai <huynhphap100@gmail.com>
2025-04-04 11:51:41 +0700

    add itemsadder

--- src/main/java/me/orineko/pluginspigottools/api/itemsadder/ItemsAdderManager.java
+++ src/main/java/me/orineko/pluginspigottools/api/itemsadder/ItemsAdderManager.java
@@ -1,6 +1,5 @@
 package me.orineko.pluginspigottools.api.itemsadder;
 
-import dev.lone.itemsadder.api.CustomStack;
 import lombok.NonNull;
 import org.bukkit.inventory.ItemStack;
 
@@ -10,9 +9,17 @@ public class ItemsAdderManager {
 
     @Nullable
     public ItemStack getItem(@NonNull String id) {
-        CustomStack customStack = CustomStack.getInstance(id);
+        try {
+            Class<?> customStackClass = Class.forName("dev.lone.itemsadder.api.CustomStack");
+            Object customStack = customStackClass.getMethod("getInstance", String.class).invoke(null, id);
+            if(customStack == null) return null;
+            return (ItemStack) customStack.getClass().getMethod("getItemStack").invoke(customStack);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        /*CustomStack customStack = CustomStack.getInstance(id);
         if(customStack == null) return null;
-        return customStack.getItemStack();
+        return customStack.getItemStack();*/
     }
 
 }
